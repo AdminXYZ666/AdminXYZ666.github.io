ff7829beef1fc17b53750d9cfe740b9aa8716eca
AdminXYZ666 <1210727679@qq.com>
2021-03-28 12:46:39 +0800

    Site updated: 2021-03-28 12:46:39

--- dist/music.js
+++ dist/music.js
@@ -1,12 +1,28 @@
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
     fixed: true,
-    autoplay: false,
+    autoplay: true,
     audio: [
 	{
         name: "Admin小谢",
         artist: 'QQ:1210727679',
         url: 'https://adminxyz666.github.io/dist/音乐/Mea .mp3',
+        cover: 'https://adminxyz666.github.io/imgs/lzlz.jpg',
+      }
+    ]
+    audio: [
+	{
+        name: "Admin小谢",
+        artist: 'QQ:1210727679',
+        url: 'https://adminxyz666.github.io/dist/音乐/陈致逸、HOYO-MiX - Another Hopeful Tomorrow 希望的明日.flac',
+        cover: 'https://adminxyz666.github.io/imgs/lzlz.jpg',
+      }
+    ]
+    audio: [
+	{
+        name: "Admin小谢",
+        artist: 'QQ:1210727679',
+        url: 'https://adminxyz666.github.io/dist/音乐/陈致逸、HOYO-MiX-Liyue璃月.m4a',
         cover: 'https://adminxyz666.github.io/imgs/lzlz.jpg',
       }
     ]
