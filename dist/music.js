b9254db5c4fdcbb436ff22aa38c107e27f62edb0
AdminXYZ666 <1210727679@qq.com>
2021-04-04 21:54:07 +0800

    Site updated: 2021-04-04 21:54:01

--- dist/music.js
+++ dist/music.js
@@ -9,6 +9,36 @@ const ap = new APlayer({
         url: 'https://adminxyz666.github.io/dist/音乐/Mea .mp3',
         cover: 'https://adminxyz666.github.io/imgs/lzlz.jpg',
       },
+                {
+        name: "Rapid as Wildfires 疾如猛火",
+        artist: '陈致逸、HOYO-MiX',
+        url: 'https://adminxyz666.github.io/dist/音乐/陈致逸、HOYO-MiX-RapidasWildfires疾如猛火.m4a',
+        cover: 'https://adminxyz666.github.io/imgs/lzlz.jpg',
+      },
+                {
+        name: "Gallant Challenge 鲜衣游侠",
+        artist: '陈致逸、HOYO-MiX',
+        url: 'https://adminxyz666.github.io/dist/音乐/陈致逸、HOYO-MiX - Gallant Challenge 鲜衣游侠.flac',
+        cover: 'https://adminxyz666.github.io/imgs/lzlz.jpg',
+      },
+                {
+        name: "Moonlike Smile 皎洁的笑颜",
+        artist: '陈致逸、HOYO-MiX',
+        url: 'https://adminxyz666.github.io/dist/音乐/陈致逸 HOYO-MiX - Moonlike Smile 皎洁的笑颜.mp3',
+        cover: 'https://adminxyz666.github.io/imgs/lzlz.jpg',
+      },
+                {
+        name: "Rex Incognito 尘世闲游",
+        artist: '陈致逸、HOYO-MiX',
+        url: 'https://adminxyz666.github.io/dist/音乐/陈致逸 HOYO-MiX - Rex Incognito 尘世闲游.mp3',
+        cover: 'https://adminxyz666.github.io/imgs/lzlz.jpg',
+      },
+	{
+        name: "Liyue璃月",
+        artist: '陈致逸、HOYO-MiX',
+        url: 'https://adminxyz666.github.io/dist/音乐/陈致逸、HOYO-MiX-Liyue璃月.m4a',
+        cover: 'https://adminxyz666.github.io/imgs/lzlz.jpg',
+      },
 	{
         name: "Another Hopeful Tomorrow 希望的明日",
         artist: '陈致逸、HOYO-MiX',
@@ -16,9 +46,45 @@ const ap = new APlayer({
         cover: 'https://adminxyz666.github.io/imgs/lzlz.jpg',
       },
 	{
-        name: "Liyue璃月",
+        name: "Call It a Day in Liyue 璃月的一日将尽",
         artist: '陈致逸、HOYO-MiX',
-        url: 'https://adminxyz666.github.io/dist/音乐/陈致逸、HOYO-MiX-Liyue璃月.m4a',
+        url: 'https://adminxyz666.github.io/dist/音乐/陈致逸、HOYO-MiX - Call It a Day in Liyue 璃月的一日将尽.flac',
+        cover: 'https://adminxyz666.github.io/imgs/lzlz.jpg',
+      },
+                {
+        name: "Cozy Leisure Time (Wangshu Daytime) 品茗尝清心 (望舒昼间)",
+        artist: '陈致逸、HOYO-MiX',
+        url: 'https://adminxyz666.github.io/dist/音乐/陈致逸、HOYO-MiX - Cozy Leisure Time (Wangshu Daytime) 品茗尝清心 (望舒昼间).flac',
+        cover: 'https://adminxyz666.github.io/imgs/lzlz.jpg',
+      },
+                {
+        name: "Faint Tracks 追寻仙迹",
+        artist: '陈致逸、HOYO-MiX',
+        url: 'https://adminxyz666.github.io/dist/音乐/陈致逸、HOYO-MiX - Faint Tracks 追寻仙迹.flac',
+        cover: 'https://adminxyz666.github.io/imgs/lzlz.jpg',
+      },
+                {
+        name: "Good Night, Liyue 晚安，璃月",
+        artist: '陈致逸、HOYO-MiX',
+        url: 'https://adminxyz666.github.io/dist/音乐/陈致逸、HOYO-MiX - Good Night, Liyue 晚安，璃月.flac',
+        cover: 'https://adminxyz666.github.io/imgs/lzlz.jpg',
+      },
+                {
+        name: "Moon in One's Cup 杯中明月",
+        artist: '陈致逸、HOYO-MiX',
+        url: 'https://adminxyz666.github.io/dist/音乐/陈致逸、HOYO-MiX - Moon in One's Cup 杯中明月.flac',
+        cover: 'https://adminxyz666.github.io/imgs/lzlz.jpg',
+      },
+                {
+        name: "The Realm Within 壶中洞天",
+        artist: '陈致逸、HOYO-MiX',
+        url: 'https://adminxyz666.github.io/dist/音乐/陈致逸、HOYO-MiX - The Realm Within 壶中洞天.flac',
+        cover: 'https://adminxyz666.github.io/imgs/lzlz.jpg',
+      },
+                {
+        name: "This Little Light of Mine 小小的光",
+        artist: '陈致逸、HOYO-MiX',
+        url: 'https://adminxyz666.github.io/dist/音乐/陈致逸、HOYO-MiX - This Little Light of Mine 小小的光.flac',
         cover: 'https://adminxyz666.github.io/imgs/lzlz.jpg',
       }
     ]
