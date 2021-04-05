8a8a925658e495b47e4c7b8021ef3ce4a7fd4e48
AdminXYZ666 <1210727679@qq.com>
2021-04-05 11:35:49 +0800

    Site updated: 2021-04-05 11:35:49

--- dist/music.js
+++ dist/music.js
@@ -39,7 +39,48 @@ const ap = new APlayer({
         url: 'https://adminxyz666.github.io/dist/音乐/陈致逸、HOYO-MiX-Liyue璃月.m4a',
         cover: 'https://adminxyz666.github.io/imgs/lzlz.jpg',
       },
-
+	{
+        name: "Another Hopeful Tomorrow 希望的明日",
+        artist: '陈致逸、HOYO-MiX',
+        url: 'https://adminxyz666.github.io/dist/音乐/陈致逸、HOYO-MiX - Another Hopeful Tomorrow 希望的明日.flac',
+        cover: 'https://adminxyz666.github.io/imgs/lzlz.jpg',
+      },
+	{
+        name: "Call It a Day in Liyue 璃月的一日将尽",
+        artist: '陈致逸、HOYO-MiX',
+        url: 'https://adminxyz666.github.io/dist/音乐/陈致逸、HOYO-MiX - Call It a Day in Liyue 璃月的一日将尽.flac',
+        cover: 'https://adminxyz666.github.io/imgs/lzlz.jpg',
+      },
+                {
+        name: "Cozy Leisure Time 品茗尝清心",
+        artist: '陈致逸、HOYO-MiX',
+        url: 'https://adminxyz666.github.io/dist/音乐/陈致逸、HOYO-MiX - Cozy Leisure Time 品茗尝清心.flac',
+        cover: 'https://adminxyz666.github.io/imgs/lzlz.jpg',
+      },
+                {
+        name: "Faint Tracks 追寻仙迹",
+        artist: '陈致逸、HOYO-MiX',
+        url: 'https://adminxyz666.github.io/dist/音乐/陈致逸、HOYO-MiX - Faint Tracks 追寻仙迹.flac',
+        cover: 'https://adminxyz666.github.io/imgs/lzlz.jpg',
+      },
+                {
+        name: "Good Night, Liyue 晚安，璃月",
+        artist: '陈致逸、HOYO-MiX',
+        url: 'https://adminxyz666.github.io/dist/音乐/陈致逸、HOYO-MiX - Good Night, Liyue 晚安，璃月.flac',
+        cover: 'https://adminxyz666.github.io/imgs/lzlz.jpg',
+      },
+                {
+        name: "Moon in One's Cup 杯中明月",
+        artist: '陈致逸、HOYO-MiX',
+        url: 'https://adminxyz666.github.io/dist/音乐/陈致逸、HOYO-MiX - Moon in One's Cup 杯中明月.flac',
+        cover: 'https://adminxyz666.github.io/imgs/lzlz.jpg',
+      },
+                {
+        name: "The Realm Within 壶中洞天",
+        artist: '陈致逸、HOYO-MiX',
+        url: 'https://adminxyz666.github.io/dist/音乐/陈致逸、HOYO-MiX - The Realm Within 壶中洞天.flac',
+        cover: 'https://adminxyz666.github.io/imgs/lzlz.jpg',
+      },
                 {
         name: "This Little Light of Mine 小小的光",
         artist: '陈致逸、HOYO-MiX',
